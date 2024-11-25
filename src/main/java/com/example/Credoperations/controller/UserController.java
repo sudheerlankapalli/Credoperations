1f2c62f3ebc06dc19bd4d892582bd24dc23bf3a2
sudheerlankapalli <sudheerlankapalli55@gmail.com>
2024-11-25 15:42:48 +0530

    Added the code to sudheer

---
+++ src/main/java/com/example/Credoperations/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.Credoperations.controller;
+
+
+import com.example.Credoperations.entity.User;
+import com.example.Credoperations.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
+
---
+++ src/main/java/com/example/Credoperations/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.Credoperations.service;
+
+
+import com.example.Credoperations.entity.User;
+import com.example.Credoperations.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setName(userDetails.getName());
+        user.setPass(userDetails.getPass());
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
