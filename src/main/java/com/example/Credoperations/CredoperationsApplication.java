aba823bab30c5719093caf3c268ba11a1dd10554
sudheerlankapalli <sudheerlankapalli55@gmail.com>
2024-11-25 15:31:19 +0530

    first commit

---
+++ src/main/java/com/example/Credoperations/CredoperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Credoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredoperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredoperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Credoperations/CredoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Credoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
