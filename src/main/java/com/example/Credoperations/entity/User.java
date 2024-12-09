e35fe8e60a54d2368aca8b9ed586759b1b3a92f9
sudheerlankapalli <sudheerlankapalli55@gmail.com>
2024-12-09 23:07:14 +0530

    Added Procfile

--- src/main/java/com/example/Credoperations/entity/User.java
+++ src/main/java/com/example/Credoperations/entity/User.java
@@ -3,26 +3,23 @@ package com.example.Credoperations.entity;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     private String name;
     private String pass;
 
     // Getters and Setters
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
